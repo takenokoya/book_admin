3da86b00b65b8f915a451007e9a91e63fd1b33e6
takenokoya <takuya1002@gmail.com>
2020-09-01 01:12:22 +0900

    add model Book, Author, BookAuthor, Publisher

---
+++ app/models/book.rb
@@ -0,0 +1,7 @@
+class Book < ApplicationRecord
+  belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+  scope :costly, -> { where('price > ?', 3000) }
+  scope :written_about, -> (theme) { where('name like ?', "%#{theme}%") }
+end
