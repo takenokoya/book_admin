ee579facd5cb5073efb7192c09f73214d043c7e6
takenokoya <takuya1002@gmail.com>
2020-09-01 01:30:29 +0900

    add some  validation and callbacks

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,25 @@ class Book < ApplicationRecord
   has_many :authors, through: :book_authors
   scope :costly, -> { where('price > ?', 3000) }
   scope :written_about, -> (theme) { where('name like ?', "%#{theme}%") }
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  # 独自のバリデーション
+  validate do |book|
+    if book.name.include?('exercise')
+      book.errors[:name] << "I don't like exercise."
+    end
+  end
+  # callbacks
+  before_validation :add_lovely_to_cat
+  after_destroy :leave_delete_log
+
+  def add_lovely_to_cat
+    self.name = self.name.gsub(/Cat/) do |matched|
+      "lovely #{matched}"
+    end
+  end
+
+  def leave_delete_log
+    Rails.logger.info "Book is deleted: #{self.attributes}"
+  end
 end
